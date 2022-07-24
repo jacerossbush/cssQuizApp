2208fbe74854134ef89b07adf677461c92ddcb05
jacerossbush <97054916+jacerossbush@users.noreply.github.com>
2022-07-23 20:49:31 -0600

    initiate quiz app
    -styles to test padding/margin
    -html layout

---
+++ script.js
@@ -0,0 +1,27 @@
+const questionsQuiz = [
+    {
+        question: 'Which of the following is not a real ecommerce platform?',
+        answerA: 'Shopify',
+        answerB: 'WooCommerce',
+        answerC: 'ShopCommerce',
+        answerD: 'BigCommerce',
+    },
+    {
+        question: 'If Shopify is so good, why are Shopify developers necessary?',
+        answerA: 'To save time on things like store setups and migrations',
+        answerB: 'to extend the limited design options and functionalities of themes with custom code',
+        answerC: 'To provide support with a deep understanding of how the platform works and what its limitations are',
+        answerD: 'All the above',
+        correctAnswer: 'answerD' 
+    },
+    {
+        question: 'Which of the following is true about Shopify Developers?',
+        answerA: 'They are paid extremely well',
+        answerB: 'There is a high demand for them',
+        answerC: 'They need to know web development, the platform itself, and the liquid template language',
+        answerD: 'All the above',
+        correctAnswer: 'answerD'
+    }
+];
+
+var questions = document.createAttribute()
